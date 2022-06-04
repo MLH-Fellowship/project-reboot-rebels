e314ea9518ec4cc162ca2708ad860fc1a67b67ab
roa-beep <roa.brahimi@mail.utoronto.ca>
2022-06-04 15:57:00 -0400

    base inheritance

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="Cyber Sapiens", url=os.getenv("URL"))
