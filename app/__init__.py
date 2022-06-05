6b1fc9ab239c011d947e9cfcc0ae0e6006c2fcd3
roa-beep <roa.brahimi@mail.utoronto.ca>
2022-06-05 14:05:42 -0400

    host change

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="Cyber Sapiens", url=os.getenv("URL"))
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host="0.0.0.0") 
