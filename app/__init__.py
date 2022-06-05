cba29d2296ae398e1899b0b3cf2d30a8b6094a07
roa-beep <roa.brahimi@mail.utoronto.ca>
2022-06-05 18:01:16 -0400

    backend handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,25 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from data import profiles
 
 load_dotenv()
 app = Flask(__name__)
 
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html', timeline_profiles=profiles)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html', title="Cyber Sapiens", url=os.getenv("URL"))
+    return render_template("index.html", title="Cyber Sapiens", url=os.getenv("URL"))
+
+
+@app.route("/about")
+def about_us():
+    return render_template("about.html")
+
 
 if __name__ == '__main__':
     app.debug = True
