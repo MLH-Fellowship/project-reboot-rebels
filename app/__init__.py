52e7a8f82f5f542d40a88b84c23dd0f9675eb1a8
roa-beep <roa.brahimi@mail.utoronto.ca>
2022-06-27 00:32:24 -0400

    part 2 timeline get and post

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,35 @@ import os
 from flask import Flask, render_template, request, json
 from dotenv import load_dotenv
 import json
+from peewee import *
+from playhouse.shortcuts import model_to_dict
+import datetime
 
 load_dotenv()
 app = Flask(__name__)
+
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+                      user=os.getenv("MYSQL_USER"),
+                      password=os.getenv("MYSQL_PASSWORD"),
+                      host=os.getenv("MYSQL_HOST"),
+                      port=3306
+                      )
+print(mydb)
+
+
+ class TimelinePost(Model):
+     name = CharField()
+     email = CharField()
+     content = TextField()
+     created_at = DateTimeField(default=datetime.datetime.now)
+
+     class Meta:
+         database = mydb
+
+
+ mydb.connect()
+ mydb.create_tables([TimelinePost])
+
 profile_file = open("app/static/data/profile.json")
 profile = json.load(profile_file)
 
@@ -59,6 +85,29 @@ def hobbiesp():
 @app.route("/maps")
 def map():
     return render_template("maps.html", apikey=os.getenv("mapkey"))
+
+
+
+@app.route("/api/timeline_post", methods=['POST'])
+def post_time_line_post():
+     name = request.form['name']
+     email = request.form['email']
+     content = request.form['content']
+     timeline_post = TimelinePost.create(
+         name=name, email=email, content=content)
+
+     return model_to_dict(timeline_post)
+
+
+@app.route("/api/timeline_post", methods=['GET'])
+def get_time_line_post():
+     return {
+         'timeline_posts': [
+             model_to_dict(p)
+             for p in
+             TimelinePost.select().order_by(TimelinePost.created_at.desc())
+         ]
+     }
 
 if __name__ == "__main__":
     app.debug = True
