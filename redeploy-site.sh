404c607872007be98feda025141ea74e4bfe8791
roa-beep <roa.brahimi@mail.utoronto.ca>
2022-07-08 17:39:10 -0400

    minor fix

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,9 +2,7 @@
 
 cd ./cyber-sapiens
 git fetch && git reset origin/main --hard
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+
 #docker compose
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
