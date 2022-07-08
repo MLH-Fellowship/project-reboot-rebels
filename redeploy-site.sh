3e740c4c1cf27b6af3076c2c569cc80746138023
roa-beep <roa.brahimi@mail.utoronto.ca>
2022-07-08 16:27:35 -0400

    docker compose update

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -5,6 +5,6 @@ git fetch && git reset origin/main --hard
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-systemctl daemon-reload
-systemctl restart myportfolio
-systemctl status myportfolio
+#docker compose
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
