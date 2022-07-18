88ba7ceea2fb9f86ccc1875b6eec9078ef001f16
roa-beep <roa.brahimi@mail.utoronto.ca>
2022-07-18 11:07:44 -0400

    cleaning repo

--- curl-test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -request POST http://127.0.0.1:5000/api/timeline_post -d 'name=Roa&email=brahimiroa@gmail.com&content=Testing!' && curl --request GET http://127.0.0.1:5000/api/timeline_post | jq '.'
