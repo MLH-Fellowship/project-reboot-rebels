37b8ace7b38c5eac69af8a62d9ef55364cad0d97
Roa Brahimi <83846594+roa-beep@users.noreply.github.com>
2022-07-18 18:23:15 -0400

    Revert "GitHub Actions test - Portfolio Update"

--- app/__init__.py
+++ app/__init__.py
@@ -2,40 +2,9 @@ import os
 from flask import Flask, render_template, request, json
 from dotenv import load_dotenv
 import json
-import requests
-from flask_cors import cross_origin, CORS
-from peewee import *
-from playhouse.shortcuts import model_to_dict
-import datetime
-from flask import Response
 
 load_dotenv()
-app = Flask(__name__, template_folder='templates')
-
-if os.getenv("TESTING") == "true":
-    print("Running in test mode")
-    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared',uri=True)
-else:
-    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
-        user=os.getenv("MYSQL_USER"),
-        password=os.getenv("MYSQL_PASSWORD"),
-        host=os.getenv("MYSQL_HOST"),
-        port=3306
-    )
-
-class TimelinePost(Model):
-    name = CharField()
-    email = CharField()
-    content = TextField()
-    created_at = DateTimeField(default=datetime.datetime.now)
-
-    class Meta:
-        database = mydb
-
-
-mydb.connect()
-mydb.create_tables([TimelinePost])
-
+app = Flask(__name__)
 profile_file = open("app/static/data/profile.json")
 profile = json.load(profile_file)
 
@@ -60,29 +29,29 @@ phobbies = json.load(phobbies_file)
 
 @app.route("/")
 def index():
-    return render_template("index.html", url=os.getenv("URL"))
+    return render_template("index.html", title="Cyber Sapiens", url=os.getenv("URL"))
 
 
-@app.route("/mytimeline")
-def mytimeline():
-    return render_template("mytimeline.html", mytimeline_profiles=profile)
+@app.route("/timeline")
+def timeline():
+    return render_template("timeline.html", timeline_profiles=profile)
 
 
-@app.route("/mytimeline/Roa/pro")
+@app.route("/timeline/Roa/pro")
 def rexperience():
     return render_template("rexp.html", rexperience=experience, redu=education)
 
 
-@app.route("/mytimeline/Pedro/pro")
+@app.route("/timeline/Pedro/pro")
 def experiencep():
     return render_template("pexp.html", pexperience=pexperience, pedu=peducation)
 
 
-@app.route("/mytimeline/Roa/hobbies")
+@app.route("/timeline/Roa/hobbies")
 def hobbiesr():
     return render_template("rhobbies.html", rhobbies=rhobbies)
 
-@app.route("/mytimeline/Pedro/hobbies")
+@app.route("/timeline/Pedro/hobbies")
 def hobbiesp():
     return render_template("phobbies.html", phobbies=phobbies)
 
@@ -91,42 +60,6 @@ def hobbiesp():
 def map():
     return render_template("maps.html", apikey=os.getenv("mapkey"))
 
-
-
-@app.route("/api/timeline_post", methods=['POST'])
-def post_time_line_post():
-    name = request.form.get('name')
-    email = request.form.get('email')
-    content = request.form.get('content')
-    if name == '' or name is None:
-        return 'Invalid name', 400
-    elif content == '' or content is None:
-        return 'Invalid content', 400
-    elif email == '' or email is None or '@' not in email:
-        return 'Invalid email', 400
-    else:
-        timeline_post = TimelinePost.create(
-            name = name,
-            email = email,
-            content = content
-        )
-        return model_to_dict(timeline_post)
-
-
-@app.route("/api/timeline_post", methods=['GET'])
-def get_time_line_post():
-    return {
-        'timeline_posts': [
-            model_to_dict(p)
-            for p in
-            TimelinePost.select().order_by(TimelinePost.created_at.desc())
-        ]
-    }
-
-@app.route("/timeline")
-def timeline():
-    timeline = get_time_line_post()["timeline_posts"]
-    return render_template('timeline.html', title="Timeline", url=os.getenv("URL"), timeline=timeline)
 
 if __name__ == "__main__":
     app.debug = True
